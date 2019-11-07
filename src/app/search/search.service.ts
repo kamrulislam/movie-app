6b37c43d933cbf63ab5e61ce9650074e76e8e426
Md Kamrul Islam <kamruli@gmail.com>
2019-11-07 14:05:03 +1100

    updated to relative path

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { SearchResult } from '.';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,7 @@ export class SearchService {
 
   fetchMovies(title: string, page: number = 1): Observable<SearchResult> {
     return this.http.get<SearchResult>(
-      `http://www.omdbapi.com/?apikey=${environment.omdbApiKey}&s=${title}&page=${page}`
+      `https://www.omdbapi.com/?apikey=${environment.omdbApiKey}&s=${title}&page=${page}`
     );
   }
 }
